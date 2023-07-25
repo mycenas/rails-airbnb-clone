83fd71d4bae70a7b7c82e19574032aae247e6a1f
David Lee <davidedwardlee@gmail.com>
2023-07-25 22:18:19 +1000

    bookings controller v1

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,13 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @bookings = Booking.find(params[:id])
+  end
+
+  def edit
+    @bookings = Booking.find(params[:id])
+  end
 end
