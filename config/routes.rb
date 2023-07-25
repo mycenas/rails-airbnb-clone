4cd5f3c9d46cca8f2f1987d6672086766fe0af65
Marnie Mycena <marniemycena@gmail.com>
2023-07-25 20:18:55 +1000

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
 Rails.application.routes.draw do
   root 'quests#index'
+
+  # DM creates a quest
+  get 'quests/new', to: 'quests#new', as: 'new_quest'
+  post '/quests', to: 'quests#create', as: 'quests'
+
+  # adventurer views available quests
+  get '/quests', to: "quests#index"
+
+  # adventurer views specific quest
+  get '/quests/:id', to: 'quests#show', as: 'quest'
+
+  #  adventurer books a quest
+  get '/quests/:id/bookings/new', to: 'bookings#show', as: 'booking'
+
+  # dm views booking requests
+  get '/bookings', to: 'bookings#index'
   
-  resources :users
-  resources :quests
-  resources :bookings
+  # dm views a specific booking request
+  get '/bookings/:id', to: 'bookings#show'
+
+  # dm accepts/rejects a booking request
+  post '/bookings/:id/accept', to:'bookings#accept', as: 'accept_booking'
+  post '/bookings/:id/decline', to: 'bookings#decline', as: 'decline_booking'
 end
