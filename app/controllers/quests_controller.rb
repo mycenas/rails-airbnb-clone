18863a372b5e01b771c0bd67f326be77e42f7cfb
Brian Hicarte <bhicarte@hotmail.com>
2023-07-27 18:53:15 +1000

    quests controller created

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,2 +1,26 @@
 class QuestsController < ApplicationController
+
+  def index
+    @quests = Quest.all
+  end
+
+  def new
+    @new_quest = Quest.new
+  end
+
+  def create
+    @quest = Quest.new(params[quest_params])
+    @quest.save
+  end
+
+  def show
+    @quest = Quest.find(params[:id])
+  end
+
+  private
+
+  def quest_params
+    params.require(:quest).permit(:name, :description, :requirement, :location, :reward, :user)
+  end
+
 end
