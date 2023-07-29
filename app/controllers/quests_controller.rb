dd832990e2d442f832a01c65c2d825bf1aef2b38
David Lee <davidedwardlee@gmail.com>
2023-07-29 14:40:21 +1000

    completed show page function

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,5 +1,5 @@
 class QuestsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  # skip_before_action :authenticate_user!, only: :index
 
   def index
     @quests = Quest.all
