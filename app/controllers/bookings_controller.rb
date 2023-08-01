0b625411aaabdd01c23a82523e5cf13ad5b15b58
Marnie Mycena <marniemycena@gmail.com>
2023-08-01 18:56:26 +1000

    coded bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,44 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :accept, :decline]
+
   def index
     @bookings = Booking.all
   end
 
-  def show
-    @bookings = Booking.find(params[:id])
+  def new
+    @booking = Booking.new
   end
 
-  def edit
-    @bookings = Booking.find(params[:id])
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to bookings_path, notice: 'Booking was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def accept
+    @booking.update(status: "accepted")
+    redirect_to bookings_path, notice: 'Booking was successfully accepted.'
+  end
+
+  def decline
+    @booking.update(status: "declined")
+    redirect_to bookings_path, notice: 'Booking was successfully declined.'
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:quest_id, :user_id, :status)
   end
 end
